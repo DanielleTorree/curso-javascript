d14f176aea66765b2a626afe8973308e462b8bc0
Danielle Torres de Freitas <danielletorree01@gmail.com>
2021-03-29 18:48:16 -0300

    Update index.js

--- exercicios/poo/index.js
+++ exercicios/poo/index.js
@@ -10,19 +10,39 @@ class Cliente{
 class ContaCorrente{
     agencia;
     saldo;
+
+
+    //sacar == comportamento (método)
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor > 0){
+            this.saldo += valor;
+        }
+    }
 };
 
 const cliente1 = new Cliente();
 cliente1.nome = "Danielle";
-cliente1.cpf = 17320610760;
-
-const cliente2 = new Cliente();
-cliente2.nome = "Alex";
-cliente2.cpf = 36131957630;
+// cliente1.cpf = 17320610760;
+// console.log(cliente1);
 
 const contaCorrenteDanielle = new ContaCorrente();
 contaCorrenteDanielle.saldo = 0;
 contaCorrenteDanielle.agencia = 1001;
 
-console.log(cliente1);
-console.log(cliente2);
+console.log(contaCorrenteDanielle.saldo);
+contaCorrenteDanielle.depositae += 100;
+contaCorrenteDanielle.. += -1;
+console.log(contaCorrenteDanielle.saldo);
+contaCorrenteDanielle.sacar(50);
+console.log(contaCorrenteDanielle.saldo);
+
+// const cliente2 = new Cliente();
+// cliente2.nome = "Alex";
+// cliente2.cpf = 36131957630;
+// console.log(cliente2);
